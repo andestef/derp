38a03456453c95576fe4b35ff2b44f6772528a25
andestef <alexander.destefano@gmail.com>
2024-12-11 15:06:18 -0500

    Added registry and config

--- Program.cs
+++ Program.cs
@@ -1,10 +1,37 @@
-﻿namespace derp
+﻿using Microsoft.Win32;
+using System.Text.Json;
+using System.IO;
+namespace derp
 {
     internal class Program
     {
+        static string derp_directory = "";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Get derp_directory from Registry (Windows only for the moment)
+            using (RegistryKey key = Registry.LocalMachine.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\App Paths\\derp.exe", false))
+            {
+                if (key == null)
+                {
+                    Console.WriteLine("Error: Derp rigestry key is undefined. Please follow correct setup instructions.");
+                    System.Environment.Exit(1);
+                }
+                else
+                {
+                    derp_directory = key.GetValue("") as string;
+                    derp_directory = derp_directory.Replace("derp.exe", "");
+                }
+            }
+            // Open Config/folders.json
+            StreamReader sr = new StreamReader(derp_directory+"\\config\\folders.json");
+            string file = sr.ReadLine();
+            while (file != null)
+            {
+                file += '\n'+sr.ReadLine();
+            }
+            sr.Close();
+            Dictionary<string,string> weatherForecast =
+                JsonSerializer.Deserialize<Dictionary<string, string>>(file);
         }
     }
 }
