50f6825fbd72da3a9237d1a085479413006612ea
andestef <alexander.destefano@gmail.com>
2024-12-12 17:53:27 -0500

    Set up batch file

--- Program.cs
+++ Program.cs
@@ -8,31 +8,36 @@ namespace derp
         static string derp_directory = "";
         static void Main(string[] args)
         {
-            // Get derp_directory from Registry (Windows only for the moment)
-            using (RegistryKey key = Registry.LocalMachine.OpenSubKey("Software\\derp", false))
+            if (args[0] == "DERPBAT")
             {
-                if (key == null)
+                // Get derp_directory from Registry (Windows only for the moment)
+                using (RegistryKey key = Registry.LocalMachine.OpenSubKey("Software\\derp", false))
                 {
-                    Console.WriteLine("Error: Derp rigestry key is undefined. Please follow correct setup instructions.");
-                    System.Environment.Exit(1);
+                    if (key == null)
+                    {
+                        Console.WriteLine("Error: Derp rigestry key is undefined. Please follow correct setup instructions.");
+                        System.Environment.Exit(1);
+                    }
+                    else
+                    {
+                        derp_directory = key.GetValue("") as string;
+                    }
                 }
-                else
+                // Open Config/folders.json
+                StreamReader sr = new StreamReader(derp_directory + "\\config\\folders.json");
+                string line = sr.ReadLine();
+                string file = "";
+                while (line != null)
                 {
-                    derp_directory = key.GetValue("") as string;
+                    file += line;
+                    line = sr.ReadLine();
                 }
+                sr.Close();
+                Dictionary<string, string> folders_data = JsonSerializer.Deserialize<Dictionary<string, string>>(file);
+                string start = args[1];
+                args = args.Skip(2).ToArray();
+                Console.WriteLine(start);
             }
-            // Open Config/folders.json
-            StreamReader sr = new StreamReader(derp_directory+"\\config\\folders.json");
-            string line = sr.ReadLine();
-            string file = "";
-            while (line != null)
-            {
-                file += line;
-                line = sr.ReadLine();
-            }
-            sr.Close();
-            Dictionary<string,string> folders_data = JsonSerializer.Deserialize<Dictionary<string, string>>(file);
-            Console.WriteLine(folders_data["~"]);
         }
     }
 }
